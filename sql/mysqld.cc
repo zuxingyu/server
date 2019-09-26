b7e4706ba4eb5de69cc85535bafc60d9625ebcfc
Daniel Black <daniel@linux.ibm.com>
2019-09-27 08:48:27 +1000

    signal handler: SI_KERNEL not define on Solaris
    Solaris doesn't have a SI_KERNEL code for its siginfo code
    structure and doesn't have an equivalent.
    
    So this prevents a compile error.
    
    ref: https://docs.oracle.com/cd/E36784_01/html/E36873/siginfo.h-3head.html

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -3282,7 +3282,11 @@ pthread_handler_t signal_hand(void *arg __attribute__((unused)))
       }
       break;
     case SIGHUP:
+#ifdef SI_KERNEL
       if (!abort_loop && origin != SI_KERNEL)
+#else
+      if (!abort_loop)
+#endif
       {
         int not_used;
 	mysql_print_status();		// Print some debug info
